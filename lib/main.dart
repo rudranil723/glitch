e31f5fb6c2183dc4c91404d85ea085ae6ddff5b6
Ruddy <rudranilshil@gmail.com>
2023-10-03 14:08:28 +0530

    workign aall good, no firebase added yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:glitch/screens/splash_screen.dart';
 
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+void main() {
   runApp(MyApp());
 }
 
